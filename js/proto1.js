466b97a8130c6b0f73fde6a031068fe4e7f8eb2b
Rock-it-science <wilmc17@gmail.com>
2022-05-12 14:42:39 -0600

    Background road

--- js/proto1.js
+++ js/proto1.js
@@ -2,27 +2,42 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
 
-function drawBackground(){
-    // Draw road
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 200, 800, 200);
-}
-
 // Draw a car moving across the screen
 carPos = 0;
 var carVel = 1; // Car velocity (keep it simple for now)
 
 // Draw canvas every frame (100/60 ms = 60 fps)
-var redrawInterval = setInterval(draw, 1000/60);
+var drawInterval = setInterval(draw, 1000/60);
 function draw(){
-    // Clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Load images
+    const roadImg = new Image();
+    roadImg.onload = function(){
+        console.log('Road image loaded');
+        // Clear canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Redraw background
-    drawBackground();
+        // Redraw background
+        
+        // Draw road
+        var roadW = roadImg.naturalWidth / 3; // Divide natural dimensions by 3 to make road proper size for canvas
+        var roadH = roadImg.naturalHeight / 3;
+        var roadX = 0; // Road x position to be iterated
+        console.log(roadW +', ' + roadH + ', ' + roadX);
+        while(roadX < canvas.width){
+            console.log(roadW +', ' + roadH + ', ' + roadX);
+            ctx.drawImage(roadImg, roadX, 200, roadW, roadH);
+            roadX += roadW;
+        }
 
-    // Car
-    ctx.fillStyle = '#FF0000';
-    ctx.fillRect(carPos, 300, 50, 20);
-    carPos += carVel;
+        // Car
+        ctx.fillStyle = '#FF0000';
+        ctx.fillRect(carPos, 300, 50, 20);
+        carPos += carVel;
+
+        // Stop movement
+        if(carPos >= 400){
+            clearInterval(drawInterval);
+        }
+    };
+    roadImg.src = 'img/istock-road.jpg';
 }
